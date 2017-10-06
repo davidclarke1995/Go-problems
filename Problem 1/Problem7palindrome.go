f35b75685ec3dbb3f54831703c317c147d55b735
DAVID CLARKE - Student <G00335563@gmit.ie>
2017-10-06 18:00:32 +0100

    Palindrome done

---
+++ Problem 1/Problem7palindrome.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+ "fmt"
+ "strings"
+)
+
+func main() {
+
+ var ip string
+ fmt.Println("Please enter a string:")
+ fmt.Scanf("%s\n", &ip)
+ ip = strings.ToLower(ip)
+ fmt.Println(isP(ip))
+}
+//Function to test if the string entered is a Palindrome or not
+
+func isP(s string) string {
+ mid := len(s) / 2
+ last := len(s) - 1
+ for i := 0; i < mid; i++ {
+  if s[i] != s[last-i] {
+   return "No this is not a Palimdrome."
+  }
+ }
+ return "Yes this is a Palindrome"
+}
